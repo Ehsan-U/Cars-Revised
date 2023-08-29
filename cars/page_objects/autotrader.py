48df736876538b7c3715e13d2bd6a44a47988c00
Ehsan U <lancerf562@gmail.com>
2023-08-29 15:26:22 +0500

    improve autotrader

--- cars/page_objects/autotrader.py
+++ cars/page_objects/autotrader.py
@@ -7,99 +7,83 @@ import json
 
 class AutoTrader_Page(CarPage):
 
-    def json_data(self):
+    def get_json_data(self):
         raw_data = self.xpath("//div[@id='mountNode']/following-sibling::script[1]/text()").get()
-        data = self.get_data(raw_data, load=True)
-        return data
+        self.json_data = self.get_data(raw_data, load=True)
 
     @field
     def source(self):
+        if not hasattr(self, 'json_data'):
+            self.get_json_data()
         return "autotrader.com"
 
     @field
     def year(self):
-        data = self.json_data()
-        return next(iter(list(data.get("initialState").get("inventory").values())), {}).get("year")
+        return next(iter(list(self.json_data.get("initialState",{}).get("inventory",[]).values())), {}).get("year")
 
     @field
     def description(self):
-        data = self.json_data()
-        return next(iter(list(data.get("initialState").get("inventory").values())), {}).get("additionalInfo").get("vehicleDescription")
+        return next(iter(list(self.json_data.get("initialState").get("inventory").values())), {}).get("additionalInfo").get("vehicleDescription")
 
     @field
     def price(self):
-        data = self.json_data()
-        return next(iter(list(data.get("initialState").get("inventory").values())), {}).get("pricingDetail").get("salePrice")
+        return next(iter(list(self.json_data.get("initialState").get("inventory").values())), {}).get("pricingDetail").get("salePrice")
 
     @field
     def comment_count(self):
-        data = self.json_data()
-        return next(iter(list(data.get("initialState").get("inventory").values())), {}).get("kbbConsumerReviewCount")
+        return next(iter(list(self.json_data.get("initialState").get("inventory").values())), {}).get("kbbConsumerReviewCount")
 
     @field
     def engine(self):
-        data = self.json_data()
-        return next(iter(list(data.get("initialState").get("inventory").values())), {}).get("specifications").get("engineDescription").get("value")
+        return next(iter(list(self.json_data.get("initialState").get("inventory").values())), {}).get("specifications").get("engineDescription").get("value")
 
     @field
     def drivetrain(self):
-        data = self.json_data()
-        return next(iter(list(data.get("initialState").get("inventory").values())), {}).get("specifications").get("driveType").get("value")
+        return next(iter(list(self.json_data.get("initialState").get("inventory").values())), {}).get("specifications").get("driveType").get("value")
 
     @field
     def mileage(self):
-        data = self.json_data()
-        return next(iter(list(data.get("initialState").get("inventory").values())), {}).get("specifications").get("mileage").get("value")
+        return next(iter(list(self.json_data.get("initialState").get("inventory").values())), {}).get("specifications").get("mileage").get("value")
 
     @field
     def vin(self):
-        data = self.json_data()
-        return next(iter(list(data.get("initialState").get("inventory").values())),{}).get("vin")
+        return next(iter(list(self.json_data.get("initialState").get("inventory").values())),{}).get("vin")
 
     @field
     def transmission(self):
-        data = self.json_data()
-        return next(iter(list(data.get("initialState").get("inventory").values())), {}).get("specifications").get("transmission").get("value")
+        return next(iter(list(self.json_data.get("initialState").get("inventory").values())), {}).get("specifications").get("transmission").get("value")
 
     @field
     def exterior(self):
-        data = self.json_data()
-        return next(iter(list(data.get("initialState").get("inventory").values())), {}).get("exteriorColorSimple")
+        return next(iter(list(self.json_data.get("initialState").get("inventory").values())), {}).get("exteriorColorSimple")
 
     @field
     def interior(self):
-        data = self.json_data()
-        return next(iter(list(data.get("initialState").get("inventory").values())), {}).get("interiorColorSimple")
+        return next(iter(list(self.json_data.get("initialState").get("inventory").values())), {}).get("interiorColorSimple")
 
     @field
     def body_style(self):
-        data = self.json_data()
-        return ", ".join(next(iter(list(data.get("initialState").get("inventory").values())), {}).get("bodyStyleCodes", []))
+        return ", ".join(next(iter(list(self.json_data.get("initialState").get("inventory").values())), {}).get("bodyStyleCodes", []))
 
     @field
     def model(self):
-        data = self.json_data()
-        return next(iter(list(data.get("initialState").get("inventory").values())), {}).get("model")
+        return next(iter(list(self.json_data.get("initialState").get("inventory").values())), {}).get("model")
 
     @field
     def make(self):
-        data = self.json_data()
-        return next(iter(list(data.get("initialState").get("inventory").values())), {}).get("make")
+        return next(iter(list(self.json_data.get("initialState").get("inventory").values())), {}).get("make")
 
     @field
     def location(self):
-        data = self.json_data()
-        return " ".join(next(iter(list(data.get("initialState").get("owners").values())), {}).get("location", {}).get("address",{}).values())
+        return " ".join(next(iter(list(self.json_data.get("initialState").get("owners").values())), {}).get("location", {}).get("address",{}).values())
 
     @field
     def seller(self):
-        data = self.json_data()
-        return next(iter(list(data.get("initialState").get("owners").values())), {}).get("name")
+        return next(iter(list(self.json_data.get("initialState").get("owners").values())), {}).get("name")
 
     @field
     def seller_type(self):
-        data = self.json_data()
-        return next(iter(list(data.get("initialState").get("owners").values())), {}).get("dealer", 'dealer')
+        return next(iter(list(self.json_data.get("initialState").get("owners").values())), {}).get("dealer", 'dealer')
 
     @field
     def reserve(self):
